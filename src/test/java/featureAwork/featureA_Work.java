bccfdd09b10d64177bd05105104bbe33f19e9418
barutcuzafer <barutcuzafer1@gmail.com>
2021-12-26 21:43:57 +0300

    Feature A1 Work

--- src/test/java/featureAwork/featureA_Work.java
+++ src/test/java/featureAwork/featureA_Work.java
@@ -3,6 +3,7 @@ package featureAwork;
 public class featureA_Work {
     public static void main(String[] args) {
         System.out.println("New Feature");
+        System.out.println("aaaaa");
     }
 
 }
