96f9c213381ea167fd61661fc737e37e517fb5f7
barutcuzafer <barutcuzafer1@gmail.com>
2021-12-26 22:33:04 +0300

    Hey man 2

--- src/test/java/Day03/Work03.java
+++ src/test/java/Day03/Work03.java
@@ -4,5 +4,6 @@ public class Work03 {
     public static void main(String[] args) {
         System.out.println("hey bro");
         System.out.println("how are you");
+        System.out.println("fuck");
     }
 }
